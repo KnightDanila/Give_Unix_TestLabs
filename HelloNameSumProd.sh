12c5cab0d6d1be8d046ab98f302439b39059c6a7
KnightDanila <KnightDanila@i.ua>
2023-02-03 10:44:28 +0200

    CW hello

---
+++ HelloNameSumProd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Enter your name: "
+read user_name
+echo "Enter your two favourite numbers: "
+read -p "Enter number one: " number_one
+read -p "Enter number two: " number_two
+sum=$(($number_one+$number_two))
+product=$(($number_one*$number_two))
+
+echo "Hi, there ^_^ "
+echo $user_name
+echo "The sum of your favourite numbers: " $sum
+echo "The product of your favourite numbers: " $product
+
+# To make script runnable on Linux use: chmod +x ./Hello.sh
